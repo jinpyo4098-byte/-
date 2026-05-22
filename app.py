6a4dab85f33708533449d0d11f5f32baede862bb
jinpyo4098-byte <jinpyo4098@gmail.com>
2026-05-22 14:36:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>12</title>
-</head>
-<body>
-    <h1>안녕</h1>
-</body>
-</html>
-
+st.title("Streamlit 테스트 중!")
+st.write("html 파일 없이 app.py로만 작동하는 사이트입니다.")
