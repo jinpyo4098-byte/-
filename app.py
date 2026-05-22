d780215297fc2755d923c1fcee354b5fca18a61b
jinpyo4098-byte <jinpyo4098@gmail.com>
2026-05-22 14:35:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>12</title>
+</head>
+<body>
+    <h1>안녕</h1>
+</body>
+</html>
+
